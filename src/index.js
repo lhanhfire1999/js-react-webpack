9b9645f6a954e2ef23d76e9835673f39a8f4fd72
Anh Le <lhanhfire1999@gmail.com>
2021-12-26 00:07:16 +0700

    build react-webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return (
+    <div>
+      <h1>Webpack 1123111</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
